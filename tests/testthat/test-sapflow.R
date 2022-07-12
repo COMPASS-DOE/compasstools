0e303443aa20b45cfd41509e098f1e6d7423cd2b
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-07-12 05:42:20 -0400

    Add read_sapflow_dir (local or Dropbox)

--- tests/testthat/test-sapflow.R
+++ tests/testthat/test-sapflow.R
@@ -1,5 +1,11 @@
 test_that("read_sapflow_file works", {
-    x <- read_sapflow_file("test_data/PNNL_11_sapflow_1min.dat")
+    x <- read_sapflow_file("test_data/PNNL_11_sapflow.dat")
     expect_s3_class(x, "data.frame")
     expect_identical(nrow(x), 5L)
+})
+
+test_that("read_sapflow_dir works locally", {
+    x <- read_sapflow_dir("test_data/")
+    expect_s3_class(x, "data.frame")
+    expect_identical(length(unique(x$Logger)), 2L)  # there are 2 files
 })
