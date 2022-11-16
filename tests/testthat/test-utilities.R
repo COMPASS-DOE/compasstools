3403b92ae3400c7142b057df4cd6e04f819f4560
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-11-16 10:52:41 -0500

    Add expand_string and expand_df (#19)
    * Add expand_string and expand_df
    * Update test-utilities.R

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -38,3 +38,64 @@ test_that("calculate_skip works", {
         suppressWarnings(calculate_skip(tf, 1, "2022-05-01", quiet = TRUE)),
         "Could not parse")
 })
+
+
+test_that("expand_string works", {
+    # No expansion
+    expect_identical("hello", expand_string("hello"))
+
+    # Comma separator
+    expect_identical(c("A", "B"), expand_string("{A,B}"))
+    expect_identical(c("CA", "CB"), expand_string("C{A,B}"))
+    expect_identical(c("AD", "BD"), expand_string("{A,B}D"))
+    expect_identical("A,B", expand_string("A,B")) # no braces
+    expect_identical("{A,B}", expand_string("{A,B}", expand_comma = FALSE))
+
+    # Colon separator
+    expect_identical(c("1", "2"), expand_string("{1:2}"))
+    expect_identical(c("C1", "C2"), expand_string("C{1:2}"))
+    expect_identical(c("1D", "2D"), expand_string("{1:2}D"))
+    expect_identical("1:2", expand_string("1:2")) # no braces
+    expect_identical("{A:B}", expand_string("{A:B}")) # no numbers
+    expect_identical("{1:2}", expand_string("{1:2}", expand_colon = FALSE))
+
+    # Nesting
+    expect_identical(c("A", "B1", "B2", "C"), expand_string("{A,B{1:2},C}"))
+
+    # Misc
+    expect_message(expand_string("AAA", quiet = FALSE), regexp = "AAA")
+})
+
+test_that("expand_df works", {
+    # No expansion
+    x <- data.frame(x = 1)
+    expect_identical(expand_df(x), x)
+    x <- dplyr::tibble(x = 1)
+    expect_identical(expand_df(x), x)
+
+    # Comma separator
+    x <- data.frame(C1 = "{A,B}", C2 = "1")
+    y <- expand_df(x)
+    expect_identical(nrow(y), 2L)
+    expect_identical(y$C1, c("A", "B"))
+    expect_identical(y$C2, c("1", "1")) # replicates
+
+    x <- data.frame(C1 = "{A,B}", C2 = "{C,D}")
+    y <- expand_df(x)
+    expect_identical(y$C2, c("C", "D")) # sequences
+
+    # Colon separator
+    x <- data.frame(C1 = "{1:2}", C2 = "1")
+    y <- expand_df(x)
+    expect_identical(nrow(y), 2L)
+    expect_identical(y$C1, c("1", "2"))
+    expect_identical(y$C2, c("1", "1")) # replicates
+
+    x <- data.frame(C1 = "{2:1}", C2 = "{C,D}")
+    y <- expand_df(x)
+    expect_identical(y$C1, c("2", "1")) # descending
+    expect_identical(y$C2, c("C", "D")) # sequences
+
+    # Nesting
+
+})
