4dd7019f2504ef4bd93ad59a455a0c04e5b12961
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-12-09 05:26:12 -0500

    Move scan_folders into this package

--- R/utilities.R
+++ R/utilities.R
@@ -159,3 +159,38 @@ expand_df <- function(df) {
 
     bind_rows(results)
 }
+
+
+#' Scan sub-folders
+#'
+#' @param root_dir Root directory to start from, character
+#' @param quiet Be quiet or print diagnostic messages? Logical
+#'
+#' @return A named list of folder contents; each list object name is the
+#' name of the folder, and each object in the list is a vector of
+#' fully qualified filenames.
+#' @note Does not recurse into sub-folders.
+#' @export
+#'
+#' @examples
+#' scan_folders("./")
+scan_folders <- function(root_dir, quiet = TRUE) {
+    if(!dir.exists(root_dir)) {
+        stop("Directory doesn't exist!")
+    }
+    entries <- list.files(root_dir, full.names = TRUE)
+
+    folder_list <- list()
+    for(e in entries) {
+        if(!quiet) message(e)
+        if(dir.exists(e)) { # it's a folder
+            files <- list.files(e, pattern = "\\.csv$", full.names = TRUE)
+            if(!quiet) message("\t", length(files), " files")
+            if(length(files)) {  # ...with csv files!
+                folder_list[[e]] <- files
+            }
+        }
+    }
+
+    return(folder_list)
+}
