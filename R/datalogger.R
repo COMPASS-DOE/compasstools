c7628606fffc54de4891a5034bc42126f2650351
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-10-17 07:11:03 -0400

    Allow user to pass ... to read_csv

--- R/datalogger.R
+++ R/datalogger.R
@@ -4,6 +4,7 @@
 #'
 #' @param filename Fully-qualified filename of a raw Campbell datalogger file
 #' @param quiet Print diagnostic messages? Logical
+#' @param ... Other parameters to pass on to \code{\link{read_csv}}
 #' @description This function reads a local file of raw sapflow data,
 #' extracts the logger number from the header, and uses
 #' \code{\link[readr]{read_csv}} to parse the file into a data frame.
@@ -15,7 +16,7 @@
 #' @examples
 #' fn <- system.file("PNNL_11_sapflow_1min.dat", package = "compasstools")
 #' read_datalogger_file(fn)
-read_datalogger_file <- function(filename, quiet = FALSE) {
+read_datalogger_file <- function(filename, quiet = FALSE, ...) {
 
     # Parse line one to extract logger and table names
     dat <- read_lines(filename)
@@ -29,9 +30,13 @@ read_datalogger_file <- function(filename, quiet = FALSE) {
     # hard-code this (unfortunately as it's much faster; see
     # https://github.com/COMPASS-DOE/compasstools/issues/12).
     # We have no time zone information, so read the timestamp as character
-    x <- read_csv(I(dat[-c(1, 3, 4)]),
-                  # don't want timestamp parsed to a datetime at this point
-                  col_types = list(TIMESTAMP = col_character()))
+    if(length(list(...))) {
+        x <- read_csv(I(dat[-c(1, 3, 4)]), ...)
+    } else {
+        x <- read_csv(I(dat[-c(1, 3, 4)]),
+                      # don't want timestamp parsed to a datetime at this point
+                      col_types = list(TIMESTAMP = col_character()))
+    }
     info <- tibble(Logger = rep(logger_name, nrow(x)),
                    Table = rep(table_name, nrow(x)),
                    Format = rep(format_name, nrow(x)))
